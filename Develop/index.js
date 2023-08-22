f7e9f476205802671c325062dfc4aaafceb76a00
kevinverduzco27 <kevin.verduzco27@gmail.com>
2023-08-22 10:55:05 -0700

    done

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,71 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const license = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {   
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+},
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+{   
+    type: 'input',
+    name: 'description',
+    message: 'Give a description of your project!'
+},
 
-// TODO: Create a function to initialize app
-function init() {}
+{   
+  type: 'input',
+  name: 'installation',
+  message: 'Enter installation instruction!'
+},
 
-// Function call to initialize app
+{   
+    type: 'input',
+    name: 'usage',
+    message: 'Please describe how your project/app/website/software is meant to be used!'
+},
+
+{   
+    type: 'checkbox',
+    name: 'license',
+    message: 'Choose a license!',
+    choices: ["Boost Software License 1.0", "BSD 3-Clause License", "Eclipse Public License 1.0", "GNU GPL v3", 
+    "The Hippocratic License 2.1", "IBM Public License Version 1.0", "ISC",  "MIT", "None"]
+},
+
+{   
+    type: 'input',
+    name: 'contribution',
+    message: 'Please list any contributors!'
+},
+
+{   
+    type: 'input',
+    name: 'gbcontact',
+    message: 'Please provide your GitHub username!'
+},
+
+{
+    type: 'input',
+    name: 'email',
+    message: 'Please provide an email to in order to be contacted!'
+}
+];
+//creats the read me
+function writeToFile(fileName, data) {
+  fs.writeFileSync(fileName, license(data), (err) =>
+err ? console.error(err) : console.log('yepperonies you did it')
+);
+}
+
+//finishes the file
+function init() {
+  inquirer
+      .prompt(questions)
+          .then(response => {
+              writeToFile('README.md', response)
+          })}
 init();
+//Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,88 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseBadge(license) {
+  if(license == "Boost Software License 1.0") {
+      return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]`
+    } else if(license == "BSD 3-Clause License") {
+      return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]`
+    } else if(license == "Eclipse Public License 1.0") {
+      return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)]`
+    } else if(license == "GNU GPL v3") {
+      return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`
+    } else if(license == "The Hippocratic License 2.1") {
+      return `[![License: Hippocratic 3.0](https://img.shields.io/badge/License-Hippocratic_2.1-lightgrey.svg)]`
+    } else if (license == "IBM Public License Version 1.0") {
+      return `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)]`
+    } else if(license == "ISC") {
+      return `[![License](https://img.shields.io/badge/License-ISC-blueviolet.svg)]`
+    } else if(license == "MIT") {
+      return `[![License](https://img.shields.io/badge/License-MIT-blueviolet.svg)]`
+    } else if(license == "None") {
+      return ""
+  }
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseLink(license) {
+  if (license == "Boost Software License 1.0") {
+      return `(https://www.boost.org/LICENSE_1_0.txt)`
+    } else if (license == "BSD 3-Clause License") {
+      return `(https://opensource.org/licenses/BSD-3-Clause)`
+    } else if (license == "Eclipse Public License 1.0") {
+      return `(https://opensource.org/licenses/EPL-1.0)`
+    } else if (license == "GNU GPL v3") {
+      return `(https://www.gnu.org/licenses/gpl-3.0)`
+    } else if (license == "The Hippocratic License 2.1") {
+      return `(https://firstdonoharm.dev)`
+    } else if (license == "IBM Public License Version 1.0") {
+      return `(https://opensource.org/licenses/IPL-1.0)`
+    } else if (license == "ISC") {
+      return `(https://opensource.org/licenses/ISC)`
+    } else if (license == "MIT") {
+      return `(https://opensource.org/licenses/MIT)`
+    } else if (license == "None") {
+      return ""
+  }
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+return `# ${data.title}
+${renderLicenseBadge(data.license)} ${renderLicenseLink(data.license)}
 
-`;
+
+## Description
+${data.description}
+
+# Table of Contents
+- [DESCRIPTION](#description)
+  
+- [USAGE](#usage)
+  
+- [INSTALLATION](#installation)
+  
+- [LICENSE](#license)
+  
+- [CREDITS/CONTRIBUTING](#contributing)
+  
+- [CONTACT/QUESTIONS](#contactGH)
+
+## Usage
+${data.usage}
+
+## Installation
+${data.installation}
+  
+## License
+${renderLicenseBadge(data.license)}
+
+${renderLicenseLink()}
+  
+## Credits/contributors:
+${data.contribution}
+
+## Questions
+
+contact me at,[Email]${data.email}) or contact me via [Github](${data.gbcontact})`;
 }
 
 module.exports = generateMarkdown;
